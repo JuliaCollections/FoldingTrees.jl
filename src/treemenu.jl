4c5c191df4da713fb4f317c10295f41b183e6ff6
Tom Short <tshort.rlists@gmail.com>
2022-01-22 17:09:17 -0500

    Add some docs

--- src/treemenu.jl
+++ src/treemenu.jl
@@ -2,6 +2,18 @@
 
 export TreeMenu
 
+"""
+    TreeMenu(root; pagesize::Int=10, dynamic = false, maxsize = pagesize, keypress = (m,i) -> false, kwargs...)
+
+Use `root` to create an interactive menu using TerminalMenus. `pagesize` is the number of lines to use for a page.
+If `dynamic`, adjust the page size based on the expansion of the content. `maxsize` is the maximum size of the page.
+
+Provide a function `keypress` to respond to keys while the menu is shown. The function has two arguments, `m::TreeMenu`
+and `i::UInt32`. The integer `i` is the key pressed. This function should return `true` if the menu should exit and
+`false` otherwise.
+
+`kwargs` are passed to `TerminalMenus.Config`.
+"""
 mutable struct TreeMenu{N<:Node} <: TerminalMenus._ConfiguredMenu{TerminalMenus.Config}
     root::N
     current::N
